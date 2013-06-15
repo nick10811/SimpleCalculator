4cf4adfc4b85eca11cb4dc7bfd707fe2b605db26
Nick Yang <nick10811@gmail.com>
2013-06-15 11:58:22 +0800

    Initial Commit

---
+++ SimpleCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleCalculator
+//
+//  Created by Nick on 13/6/15.
+//  Copyright (c) 2013年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
